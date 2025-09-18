575e9a9f6a50b76931d7669d931d0c4535d2638b
Stiven-Chacon <stivenchacon031@gmail.com>
2025-09-18 12:06:30 -0500

    feat: add ProductsGrid component to Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,19 @@
 "use client"
 
 import { Header } from "@/components/layout/header"
-import { Box } from "@mui/material"
+import { ProductsGrid } from "@/components/products/products-grid"
+import { Box, Container } from "@mui/material"
 
 export default function Home() {
   return (
     <Box sx={{ minHeight: "100vh", bgcolor: "background.default" }}>
       <Header />
+
+      <Container>
+        <ProductsGrid />
+      </Container>
+
+
     </Box>
   )
 }
